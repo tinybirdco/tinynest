ec506b060263fccda57babe79f29211796be10a0
alrocar <alrocar@tinybird.co>
2024-12-30 17:53:07 +0100

    fix

--- apps/web/src/app/api/mcp/route.ts
+++
@@ -1,6 +0,0 @@
-import WebSocketSingleton from '@/lib/websocket-server';
-
-export async function GET() {
-    await WebSocketSingleton.getInstance();
-    return new Response('WebSocket server is running', { status: 200 });
-} 
