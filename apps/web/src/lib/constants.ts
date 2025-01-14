51a9871c3513da21054899b6d1ec436090c4bf50
alrocar <alrocar@tinybird.co>
2025-01-14 12:20:55 +0100

    fix preview

--- apps/web/src/lib/constants.ts
+++ apps/web/src/lib/constants.ts
@@ -1,11 +1,6 @@
 import dynamic from 'next/dynamic';
 
 export const baseURL = (() => {
-    // Preview deployments
-    if (process.env.VERCEL_ENV === 'preview') {
-        return `https://${process.env.VERCEL_BRANCH_URL}`;
-    }
-
     // Production URL
     if (process.env.NEXT_PUBLIC_VERCEL_URL) {
         return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
